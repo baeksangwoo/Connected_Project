1f872095de675fe8ade62c26edfd77840e6a0bce
sangwoo <qortkdn@naver.com>
2018-04-03 10:01:44 +0900

    jpg 그림 파일로 로컬에 저장

---
+++ R programming/day06/r1.R
@@ -0,0 +1,3 @@
+library(ggplot2)
+
+jpeg(filename = "c:\\rproject\\mychart.jpg",width = 680,height = 680,quality = )
---
+++ R programming/day06/r10.R
@@ -0,0 +1,7 @@
+gl<-function(){
+  library(ggplot2)
+  jpeg(filename = "c:\\mychart.jpg",width = 680,height = 680,quality = 100);
+  ggplot(data=midwest, aes(x=poptotal, y=popasian))+geom_point()+xlim(0,500000)+ylim(0,10000);
+  
+  dev.off()
+}
