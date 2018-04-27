f6c1420a1dd3a44a4f20cfbb31c654b37b6b3cee
sangwoo <qortkdn@naver.com>
2018-04-27 13:35:29 +0900

    멀티 클라이언트 Accept

--- JaVA Spring 과정/day03/src/tcp2/Server.java
+++ JaVA Spring 과정/day03/src/tcp2/Server.java
@@ -6,15 +6,12 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import tcp1.Server;
 
 public class Server {
 	
 	int port;
 	ServerSocket serverSocket;
-	Socket socket;
-	OutputStream out;
-	OutputStreamWriter outw;
+
 	boolean flag =true;
 	
 	
@@ -24,17 +21,61 @@ public class Server {
 	}
 ///////////////////동일////////////////////////////////
 	
-	public void startServer() { //역할 Accept Client Socket
+	public void startServer() throws IOException {
+		System.out.println("Start Server....");
+		while(flag) {
+			Socket socket=null;
+			System.out.println("Ready Server....");
+			socket= serverSocket.accept();
+			Sender sender =new Sender(socket); //들어 올때마다 소켓을 Accept
+			new Thread(sender).start();
+			System.out.println("Accepted Client ....."+socket.getInetAddress());	
 		
-		
+		}
 	}
-	
 	class Sender implements Runnable{
-
+		OutputStream out;
+		OutputStreamWriter outw;
+		Socket socket;
+		
+		public Sender() {}
+		public Sender(Socket socket) throws IOException {
+			this.socket =socket;
+			out=socket.getOutputStream();
+			outw=new OutputStreamWriter(out);
+		}
+		
 		@Override
 		public void run() {
 			// TODO Auto-generated method stub
-			
+			try {
+				Thread.sleep(3000);
+				outw.write("안녕");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				if(outw!=null) {
+					try {
+						outw.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if(socket!=null) {
+					try {
+						socket.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
 		}
 		
 	}
