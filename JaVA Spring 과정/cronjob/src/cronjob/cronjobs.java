c7ebc79784f08aaa8595a7df427de03a6b57e763
sangwoo <qortkdn@naver.com>
2018-05-30 17:39:05 +0900

    Cron Job 설정 시작

---
+++ JaVA Spring 과정/cronjob/src/cronjob/cronjobs.java
@@ -0,0 +1,5 @@
+package cronjob;
+
+public class cronjobs {
+
+}
---
+++ JaVA Spring 과정/cronjob/src/cronjob/cronjobss.java
@@ -0,0 +1,19 @@
+package cronjob;
+
+public class cronjobss {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<0 ;i++)
+		{
+			System.out.println("Hello world");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
