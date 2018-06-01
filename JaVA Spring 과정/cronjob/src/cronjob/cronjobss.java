e1cd8a8ea1834393e07f122e991c00a952f532d0
sangwoo <qortkdn@naver.com>
2018-06-01 21:35:45 +0900

    Paging 처리

--- JaVA Spring 과정/cronjob/src/cronjob/cronjobss.java
+++ JaVA Spring 과정/cronjob/src/cronjob/cronjobss.java
@@ -1,19 +1,8 @@
 package cronjob;
 
+import java.util.Date;
+
 public class cronjobss {
 
-	public static void main(String[] args) {
-		
-		for(int i=0; i<0 ;i++)
-		{
-			System.out.println("Hello world");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
 
 }
